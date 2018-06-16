b1bc5059d1db9ae0ed9069d35399f7425991a85b
Rimo <RimoChan@stu.xmu.edu.cn>
2018-06-16 19:12:50 +0800

    添加了几个常用函数

--- pythongl.py
+++ pythongl.py
@@ -7,6 +7,7 @@ from OpenGL.GLUT import *
 
 from PIL import Image
 import os
+import numpy as np
 
 @contextlib.contextmanager
 def temp_translate(x,y,z):
@@ -19,6 +20,26 @@ def temp_scale(x,y,z):
     glScalef(x,y,z)
     yield
     glScalef(1/x,1/y,1/z)
+
+m={'x':(1,0,0),'y':(0,1,0),'z':(0,0,1)}
+@contextlib.contextmanager
+def temp_rotate(d,c):
+    glRotatef(d,*m[c])
+    yield
+    glRotatef(-d,*m[c])
+
+def vec_rotate(a,b):
+    a/=np.linalg.norm(a)
+    b/=np.linalg.norm(b)
+    v=np.cross(a,b)
+    d=np.degrees(np.arccos(np.dot(a,b)))
+    glRotatef(d,*v)
+
+@contextlib.contextmanager
+def temp_vec_rotate(a,b):
+    vec_rotate(a,b)
+    yield
+    vec_rotate(b,a)
 
 @contextlib.contextmanager
 def new_list(list_id):
